68d286b4ee15cf4239b78ffab98ac3260703f839
Willie Abrams <willie@pobox.com>
2024-02-27 08:04:42 -0600

    formatting

--- foo_library.cpp
+++ foo_library.cpp
@@ -7,31 +7,26 @@
 namespace trace = opentelemetry::trace;
 namespace nostd = opentelemetry::nostd;
 
-namespace
-{
-nostd::shared_ptr<trace::Tracer> get_tracer()
-{
-  auto provider = trace::Provider::GetTracerProvider();
-  return provider->GetTracer("foo_library", OPENTELEMETRY_SDK_VERSION);
-}
+namespace {
+	nostd::shared_ptr<trace::Tracer> get_tracer() {
+		auto provider = trace::Provider::GetTracerProvider();
+		return provider->GetTracer("foo_library", OPENTELEMETRY_SDK_VERSION);
+	}
 
-void f1()
-{
-  auto scoped_span = trace::Scope(get_tracer()->StartSpan("f1"));
-}
+	void f1() {
+		auto scoped_span = trace::Scope(get_tracer()->StartSpan("f1"));
+	}
 
-void f2()
-{
-  auto scoped_span = trace::Scope(get_tracer()->StartSpan("f2"));
+	void f2() {
+		auto scoped_span = trace::Scope(get_tracer()->StartSpan("f2"));
 
-  f1();
-  f1();
-}
-}  // namespace
+		f1();
+		f1();
+	}
+} // namespace
 
-void foo_library()
-{
-  auto scoped_span = trace::Scope(get_tracer()->StartSpan("library"));
+void foo_library() {
+	auto scoped_span = trace::Scope(get_tracer()->StartSpan("library"));
 
-  f2();
+	f2();
 }
--- main.cpp
+++ main.cpp
@@ -15,44 +15,39 @@
 
 #include <string>
 
+#include "foo_library.h"
 
-# include "foo_library.h"
-
-
-namespace trace     = opentelemetry::trace;
+namespace trace = opentelemetry::trace;
 namespace trace_sdk = opentelemetry::sdk::trace;
-namespace otlp      = opentelemetry::exporter::otlp;
+namespace otlp = opentelemetry::exporter::otlp;
 
 namespace internal_log = opentelemetry::sdk::common::internal_log;
 
-namespace
-{
-opentelemetry::exporter::otlp::OtlpHttpExporterOptions opts;
-void InitTracer()
-{
-  // Create OTLP exporter instance
-  auto exporter  = otlp::OtlpHttpExporterFactory::Create(opts);
-  auto processor = trace_sdk::SimpleSpanProcessorFactory::Create(std::move(exporter));
-  std::shared_ptr<opentelemetry::trace::TracerProvider> provider =
-      trace_sdk::TracerProviderFactory::Create(std::move(processor));
-  // Set the global trace provider
-  trace::Provider::SetTracerProvider(provider);
-}
+namespace {
+	opentelemetry::exporter::otlp::OtlpHttpExporterOptions opts;
+	
+	void InitTracer() {
+		// Create OTLP exporter instance
+		auto exporter = otlp::OtlpHttpExporterFactory::Create(opts);
+		auto processor = trace_sdk::SimpleSpanProcessorFactory::Create(std::move(exporter));
+		std::shared_ptr<opentelemetry::trace::TracerProvider> provider =
+			trace_sdk::TracerProviderFactory::Create(std::move(processor));
+		// Set the global trace provider
+		trace::Provider::SetTracerProvider(provider);
+	}
 
-void CleanupTracer()
-{
-  // We call ForceFlush to prevent to cancel running exportings, It's optional.
-  opentelemetry::nostd::shared_ptr<opentelemetry::trace::TracerProvider> provider =
-      trace::Provider::GetTracerProvider();
-  if (provider)
-  {
-    static_cast<trace_sdk::TracerProvider *>(provider.get())->ForceFlush();
-  }
+	void CleanupTracer() {
+		// We call ForceFlush to prevent to cancel running exportings, It's optional.
+		opentelemetry::nostd::shared_ptr<opentelemetry::trace::TracerProvider> provider =
+			trace::Provider::GetTracerProvider();
+		if (provider) {
+			static_cast<trace_sdk::TracerProvider *>(provider.get())->ForceFlush();
+		}
 
-  std::shared_ptr<opentelemetry::trace::TracerProvider> none;
-  trace::Provider::SetTracerProvider(none);
-}
-}  // namespace
+		std::shared_ptr<opentelemetry::trace::TracerProvider> none;
+		trace::Provider::SetTracerProvider(none);
+	}
+} // namespace
 
 /*
   Usage:
@@ -63,36 +58,30 @@ void CleanupTracer()
   <DEBUG> = yes|no, to turn console debug on or off
   <BIN> = bin, to export in binary format
 */
-int main(int argc, char *argv[])
-{
-  if (argc > 1)
-  {
-    opts.url = argv[1];
-    if (argc > 2)
-    {
-      std::string debug  = argv[2];
-      opts.console_debug = debug != "" && debug != "0" && debug != "no";
-    }
+int main(int argc, char *argv[]) {
+	if (argc > 1) {
+		opts.url = argv[1];
+		if (argc > 2) {
+			std::string debug = argv[2];
+			opts.console_debug = debug != "" && debug != "0" && debug != "no";
+		}
 
-    if (argc > 3)
-    {
-      std::string binary_mode = argv[3];
-      if (binary_mode.size() >= 3 && binary_mode.substr(0, 3) == "bin")
-      {
-        opts.content_type = otlp::HttpRequestContentType::kBinary;
-      }
-    }
-  }
+		if (argc > 3) {
+			std::string binary_mode = argv[3];
+			if (binary_mode.size() >= 3 && binary_mode.substr(0, 3) == "bin") {
+				opts.content_type = otlp::HttpRequestContentType::kBinary;
+			}
+		}
+	}
 
-  if (opts.console_debug)
-  {
-    internal_log::GlobalLogHandler::SetLogLevel(internal_log::LogLevel::Debug);
-  }
+	if (opts.console_debug) {
+		internal_log::GlobalLogHandler::SetLogLevel(internal_log::LogLevel::Debug);
+	}
 
-  // Removing this line will leave the default noop TracerProvider in place.
-  InitTracer();
+	// Removing this line will leave the default noop TracerProvider in place.
+	InitTracer();
 
-  foo_library();
+	foo_library();
 
-  CleanupTracer();
+	CleanupTracer();
 }
